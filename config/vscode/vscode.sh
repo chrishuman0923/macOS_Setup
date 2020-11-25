f5d901b99d9f9352e3c12dd5bf5aabd87ead69e2
Chris Human <chrisehuman@gmail.com>
2020-11-25 11:14:20 -0500

    Add vs code snippets

--- config/vscode/vscode.sh
+++ config/vscode/vscode.sh
@@ -5,9 +5,22 @@
 
 # Replace VS Code settings.json
 REPO_FILE="./config/vscode/settings.json"
-LOCAL_FILE="$HOME/Library/Application\ Support/Code/User/settings.json"
+LOCAL_DIR="$HOME/Library/Application Support/Code/User"
 
 echo "Updating VS Code settings.json...\n"
-cp "$REPO_FILE" "$LOCAL_FILE"
+cp "$REPO_FILE" "$LOCAL_DIR"
+
+# Add global snippets file
+REPO_FILE="./config/vscode/global.code-snippets"
+LOCAL_DIR="$HOME/Library/Application\ Support/Code/User/snippets"
+
+echo "Adding VS Code snippets...\n"
+
+# Create directory if necessary
+if [ ! -d LOCAL_DIR ]
+then mkdir -p "$LOCAL_DIR"
+fi
+
+cp "$REPO_FILE" "$LOCAL_DIR"
 
 echo "Done."
