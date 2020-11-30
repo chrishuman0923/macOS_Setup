9ba673ad16dc975b873cb4a084b5248b9ebaa9e0
Chris Human <chrisehuman@gmail.com>
2020-11-30 10:35:50 -0500

    Remove unnecessary config file
    Installed new extension to cover custom defined snippets.

--- config/vscode/vscode.sh
+++ config/vscode/vscode.sh
@@ -10,17 +10,4 @@ LOCAL_DIR="$HOME/Library/Application Support/Code/User"
 echo "Updating VS Code settings.json...\n"
 cp "$REPO_FILE" "$LOCAL_DIR"
 
-# Add global snippets file
-REPO_FILE="./config/vscode/global.code-snippets"
-LOCAL_DIR="$HOME/Library/Application\ Support/Code/User/snippets"
-
-echo "Adding VS Code snippets...\n"
-
-# Create directory if necessary
-if [ ! -d LOCAL_DIR ]
-then mkdir -p "$LOCAL_DIR"
-fi
-
-cp "$REPO_FILE" "$LOCAL_DIR"
-
 echo "Done."
